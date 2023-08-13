9ceb99c2be1ba440053c366fadad68128c090b1f
alexCoding42 <alexandrecisse.dev@gmail.com>
2023-08-13 15:36:34 -0400

    feat: :sparkles: add routes and controllers for residencies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { userRoute } from "./routes/userRoute.js";
+import { residencyRoute } from "./routes/residencyRoute.js";
 
 dotenv.config();
 
@@ -11,8 +13,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/user", userRoute);
+app.use("/api/residency", residencyRoute);
