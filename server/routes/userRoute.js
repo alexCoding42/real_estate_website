329ec099451d16ed2ad638126f31026d9112a6b6
alexCoding42 <alexandrecisse.dev@gmail.com>
2023-08-14 22:32:45 -0400

    feat: :sparkles: add favorite feature

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,6 +4,8 @@ import {
   bookVisit,
   getAllBookings,
   cancelBooking,
+  toFav,
+  getAllFavorites,
 } from "../controllers/userCntrl.js";
 const router = express.Router();
 
@@ -11,5 +13,7 @@ router.post("/register", createUser);
 router.post("/bookVisit/:id", bookVisit);
 router.post("/allBookings", getAllBookings);
 router.post("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav/", getAllFavorites);
 
 export { router as userRoute };
