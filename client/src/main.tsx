fa52066d8693dd55e7e7c3d01ed71f26c95c29c9
alexCoding42 <alexandrecisse.dev@gmail.com>
2023-09-03 22:11:12 +0200

    auth0 updated

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: "https://real-estate-website-client.vercel.app",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
